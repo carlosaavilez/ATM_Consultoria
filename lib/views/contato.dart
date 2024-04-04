b001b4720dda534458ba52bd348e24679e1ef908
3DS <3ds@bglab.local>
2024-04-04 20:34:55 -0300

    04_04 botões

---
+++ lib/views/contato.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Contato extends StatefulWidget {
+  const Contato({super.key});
+
+  @override
+  State<Contato> createState() => _ContatoState();
+}
+
+class _ContatoState extends State<Contato> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(Title: Text("Contato"))
+    );
+  }
+}
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -11,14 +11,51 @@ class _HomeAtmState extends State<HomeAtm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text("Consultoria ATM"),
-
       ),
       body: Container(
         padding: EdgeInsets.all(16),
-        child: Column(),
+        child: Column(
+          children: [
+            Image.asset("imagens/logo.png"),
+            SizedBox(
+              height: 34,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  child: Image.asset("imagens/menu_servico.png"),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                GestureDetector(
+                  child: Image.asset("imagens/menu_empresa.png"),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 34,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  child: Image.asset("imagens/menu_cliente.png"),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                GestureDetector(
+                  child: Image.asset("imagens/menu_contato.png"),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
